26cb35d1c18bd8f2c9107e69eae1b1cc9918611a
Safayet Neyam <neembukola@gmail.com>
2020-12-02 06:15:38 +0600

    Create calculating-total-divisors-of-a-number.cpp

---
+++ algorithms/calculating-total-divisors-of-a-number.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int countDivisors(int n) {
+    int count = 0;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            int a = i;
+            int b = n / i;
+
+            count++;
+
+            if (a != b) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+int main(void)
+{
+    int num, total;
+    cin >> num;
+    total = countDivisors(num);
+    cout << "Total Divisors: " << total << endl;
+
+    return 0;
+}
