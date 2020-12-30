b5546dd8d1629df1c6e2150f929db7e76a177c86
Safayet Neyam <neembukola@gmail.com>
2020-12-31 00:50:25 +0600

    Week 06

---
+++ algorithms/binarySearch_index.cpp
@@ -0,0 +1,22 @@
+int qNum;
+cin >> qNum;
+
+int res = -1;
+int left = 0, right = num - 1;
+
+while (left <= right) {
+    int mid = left + (right - left) / 2;
+            
+    if (arr[mid] == qNum) {
+        res = mid;
+        right = mid - 1;
+    } else if (arr[mid] > qNum) {
+        right = mid - 1;
+    } else {
+        left = mid + 1;
+    }
+}
+
+
+//Search Number by Index
+//Similar Problem: https://www.spoj.com/problems/BSEARCH1/
