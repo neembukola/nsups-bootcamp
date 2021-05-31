7af02373590425d2f3de5e221535f18d234dfa5e
Safayet Neyam <neembukola@gmail.com>
2021-05-31 14:08:26 +0600

    Greetings from LightOJ

---
+++ solved-problems/LightOJ_1000.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int cases;
+    cin >> cases;
+    for (int i = 1; i <= cases; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << "Case " << i << ": " << a + b << endl;
+    }
+}
+
+//https://lightoj.com/problem/greetings-from-lightoj
