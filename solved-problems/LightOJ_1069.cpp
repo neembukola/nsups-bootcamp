b83923f372b930d6854911bb65a3b7a7ee3028dc
Safayet Neyam <neembukola@gmail.com>
2021-05-31 14:06:37 +0600

    Lift

---
+++ solved-problems/LightOJ_1069.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int cases;
+    cin >> cases;
+    for (int i = 1; i <= cases; i++) {
+        int init, lift;
+        cin >> init >> lift;
+        int move = 3 * 3 + 5 * 2;
+        int time;
+        if (init > lift) {
+            time = ((init - lift) + init) * 4 + move;
+        } else {
+            time = lift * 4 + move;
+        }
+        cout << "Case " << i << ": " << time << endl;
+    }
+}
+
+//https://lightoj.com/problem/lift
