509197483451f78acf152d3aae68b8b097e511db
Safayet Neyam <neembukola@gmail.com>
2021-05-31 14:07:54 +0600

    Opposite Task

---
+++ solved-problems/LightOJ_1001.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int cases;
+    cin >> cases;
+    while (cases--) {
+        int prob;
+        cin >> prob;
+        if (prob < 10) {
+            cout << 0 << " " << prob << endl;
+        } else {
+            cout << prob - 10 << " " << 10 << endl;
+        }
+    }
+}
+
+//https://lightoj.com/problem/opposite-task
