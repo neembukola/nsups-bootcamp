2d1ca78cf38f08a1b77849f634f31133a633cfc6
Safayet Neyam <neembukola@gmail.com>
2021-06-05 01:10:02 +0600

    Prime or Non Prime

--- algorithms/prime_check.cpp
+++ algorithms/prime_check.cpp
@@ -3,30 +3,28 @@ using namespace std;
 
 bool isPrime(int n); 
 
-int main(void)
-{
+int main() {
     int num;
     cin >> num;
-    bool checkPrime = isPrime(num);
-
-    if (checkPrime) {
+    //bool checkPrime = isPrime(num);
+    //if (checkPrime) 
+    if (isPrime(num)) {
         cout << num << " is Prime." << endl;
     } else {
         cout << num << " is not Prime." << endl;
     }
-
     return 0;
 }
+
+//Algorithm: Prime or Non Prime
+//Complexity: O(sqrt(n))
 
 bool isPrime(int n) {
     if (n == 1) {
         return false;
     }
-    if (n == 2) {
-        return true;
-    }
     for (int i = 2; i * i <= n; i++) {
-        if( n % i == 0) {
+        if ( n % i == 0) {
             return false;
         }
     }
