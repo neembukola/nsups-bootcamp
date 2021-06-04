09d6d4edd42323e92f26c3fd16891a7f3eed7a14
Safayet Neyam <neembukola@gmail.com>
2021-06-05 00:48:52 +0600

    Divisors (Total/Count)

--- algorithms/divisors_count.cpp
+++ algorithms/divisors_count.cpp
@@ -3,35 +3,28 @@ using namespace std;
 
 int countDivisors(int n);
 
-int main(void)
-{
-    int num, total;
-    cin >> num;
-    total = countDivisors(num);
+int main() {
+    int n, total;
+    cin >> n;
+    total = countDivisors(n);
     cout << "Total Divisors: " << total << endl;
-
     return 0;
 }
 
-int countDivisors(int n) 
-{
-    int count = 0;
+//Algorithm: Divisors (Total/Count)
+//Algorithm: O(sqrt(n))
 
+int countDivisors(int n) {
+    int count = 0;
     for (int i = 1; i * i <= n; i++) {
         if (n % i == 0) {
             int a = i;
             int b = n / i;
-
             count++;
-
             if (a != b) {
                 count++;
             }
         }
     }
-
     return count; 
 }
-
-
-// O(sqrt(n))
