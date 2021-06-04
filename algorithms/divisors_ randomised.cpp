4f7de5e975933814b342b5cc354ebd8fa13bcb57
Safayet Neyam <neembukola@gmail.com>
2021-06-05 00:28:02 +0600

    Randomised Divisor

--- algorithms/divisors_ randomised.cpp
+++ algorithms/divisors_ randomised.cpp
@@ -3,24 +3,21 @@ using namespace std;
 
 void divisors(int n);
 
-int main(void)
-{
-    int num;
-    cin >> num;
-    divisors(num);
-
+int main() {
+    int n;
+    cin >> n;
+    divisors(n);
     return 0; 
 }
 
-void divisors(int n) 
-{
+//Algorithm
+
+void divisors(int n) {
     for (int i = 1; i * i <= n; i++) {
         if (n % i == 0) {
             int a = i;
             int b = n / i;
-
             cout << a << endl;
-
             if (a != b) {
                 cout << b << endl;
             }
