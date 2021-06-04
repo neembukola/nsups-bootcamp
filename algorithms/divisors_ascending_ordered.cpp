d714350722061b2ce0c68f3c0e99932a3de89256
Safayet Neyam <neembukola@gmail.com>
2021-06-05 00:41:05 +0600

    Divisors (Ascending Ordered)

--- algorithms/divisors_ascending_ordered.cpp
+++ algorithms/divisors_ascending_ordered.cpp
@@ -4,27 +4,28 @@ using namespace std;
 
 void divisors(int n);
 
-int main(void)
-{
+int main() {
     int n;
     cin >> n;
     divisors(n);
-    
-    return 0; 
+    return 0;
 }
 
+//Algorithm: Divisors (Ascending Ordered)
+//Algorithm: O(sqrt(n))
+
 void divisors(int n) {
-    for (int i = 1; i < sqrt(n); i++) {
-        if (n % i == 0){
-            cout << i << " ";
+    for (int i = 1; i * i <= n; i++) {
+        if ((n % i == 0) && (i * i != n)) {
+            cout << i << endl;
         }
     }
     for (int i = sqrt(n); i >= 1; i--) {
-        if (n % i == 0){
+        if (n % i == 0) {
             int div = n / i;
-            cout << div << " ";
+            cout << div << endl;
         }
     }
-
-    cout << endl;
 }
+
+//Line 19: (i * i != n) [Personal Observation]
