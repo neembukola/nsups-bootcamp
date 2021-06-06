e62658c3bf26e2aff0d909f7374a8d873cb5350a
Safayet Neyam <neembukola@gmail.com>
2021-06-06 20:13:43 +0600

    Divisors Sum of A Number / SOD

--- algorithms/divisors_sum.cpp
+++ algorithms/divisors_sum.cpp
@@ -6,27 +6,18 @@ int sum_of_divisors[MAXX];
 
 void divisors_sum();
 
-int main(void)
-{
-    int testcase;
-    cout << "Enter Testcase: ";
-    cin >> testcase;
-
-    divisors_sum();
-
-    while (testcase--) {
-        int number;
-        cout << "Enter an integer: ";
-        cin >> number;
-
-        cout << "Sum of Divisors: " << sum_of_divisors[number] << endl;
-    }
-
+int main() {
+    int number;
+    cout << "Enter an integer: ";
+    cin >> number;
+    cout << "Sum of Divisors: " << sum_of_divisors[number] << endl;
     return 0;
 }
 
-void divisors_sum()
-{
+//Algorithm: Divisors Sum of A Number / SOD
+//Algorithm: O(sqrt(n))
+
+void divisors_sum() {
     for (int i = 1; i<= MAXX; i++) {
         for (int j = i; j <= MAXX; j += i) {
             sum_of_divisors[j] += i;
@@ -34,40 +25,22 @@ void divisors_sum()
     }
 }
 
-
-
-//O(n log(n))
-
 /*
-//excluding testcase
+Direct Solution
+Complexity: O(sqrt(n))
 
-#include <iostream>
-using namespace std;
-
-const int MAXX = 1e5;
-int sum_of_divisors[MAXX];
-
-void divisors_sum(int n);
-
-int main(void)
-{
-    int number;
-    cout << "Enter an integer: ";
-    cin >> number;
-
-    divisors_sum(number);
-    cout << "Sum of Divisors: " << sum_of_divisors[number] << endl;
-
-    return 0;
-}
-
-void divisors_sum(int n)
-{
-    for (int i = 1; i<= n; i++) {
-        for (int j = i; j <= n; j += i) {
-            sum_of_divisors[j] += i;
+int sumDivisors(int n) {
+    int count = 0;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            int a = i;
+            int b = n / i;
+            count += a;
+            if (a != b) {
+                count += b;
+            }
         }
     }
+    return count; 
 }
-
 */
