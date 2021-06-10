14101efb9e42fd89bff26047ff85c7fa858216ad
Safayet Neyam <neembukola@gmail.com>
2021-06-10 21:50:10 +0600

    Prime or Non Prime

--- algorithms/prime_check.cpp
+++ algorithms/prime_check.cpp
@@ -24,7 +24,7 @@ bool isPrime(int n) {
         return false;
     }
     for (int i = 2; i * i <= n; i++) {
-        if ( n % i == 0) {
+        if (n % i == 0) {
             return false;
         }
     }
